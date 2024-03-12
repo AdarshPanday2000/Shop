5fc701b6a0eafbed6934f40d52c05c1bbf035f9d
Abhishek Singh <as9284608@gmail.com>
2024-03-12 13:49:51 +0530

    removing extra code

--- server/server.js
+++ server/server.js
@@ -1,52 +1,34 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const port = process.env.PORT;
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(cors())
-
-// app.get("/", (req,res) => {
-//     res.send("Hello world!")
-// });
-
-// app.post("/pay", async (req,res) => {
-//     console.log(req.body.token);
-//     await Stripe.charges.create({
-//         source: req.body.token.id,
-//         amount: req.body.amount,
-//         currency:"usd",
-//     })
-// })
-
-// app.listen(port, () => {
-//     console.log(`server is running on port ${port}`)
-// }) 
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // This is your test secret API key.
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const YOUR_DOMAIN = `http://localhost:${port}`;
 
-app.post('/pay', async (req, res) => {
-   await stripe.checkout.sessions.create({
+app.post("/pay", async (req, res) => {
+  await stripe.checkout.sessions.create({
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
         source: req.body.token.id,
         price: req.body.amount,
-        currency:"usd",
+        currency: "usd",
       },
     ],
-    mode: 'payment',
+    mode: "payment",
     success_url: `${YOUR_DOMAIN}?success=true`,
     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
 });
 
-app.listen(port, () => console.log('Running on port 8000'));
+app.listen(port, () => console.log("Running on port 8000"));
