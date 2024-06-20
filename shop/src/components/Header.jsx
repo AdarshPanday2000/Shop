4aa0db050c82dd8a3070bf05cc03d2f45ceb18f8
AdarshPanday2000 <117563821+AdarshPanday2000@users.noreply.github.com>
2024-06-20 13:25:48 +0530

    changes in header

--- shop/src/components/Header.jsx
+++ shop/src/components/Header.jsx
@@ -19,10 +19,7 @@ function Header() {
             <div className='flex items-center gap-8'>
               <ul className='flex items-center gap-8'>
                 <Link to='/' className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Home</Link>
-                <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Pages</li>
-                <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Shop</li>
-                <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Element</li>
-                <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Blog</li>
+
               </ul>
              <Link to='/cart'>
               <div className=' relative'>
--- shop/src/pages/Login.jsx
+++ shop/src/pages/Login.jsx
@@ -62,14 +62,14 @@ function Login() {
             </div>
             <button onClick={handleSignOut} className='bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 durTION-300'>Sign Out</button>
         </div>
-
+{/* 
         <div className='w-full flex items-center justify-center gap-10'>
             <div className='text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:hover:border-blue-600 cursor-pointer duration-300'>
                <img className='w-8' src = {githubLogo} alt="googleLogo"/>
                <span className='text-sm text-gray-900'>Sign in with Github</span>
             </div>
             <button className='bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 durTION-300'>Sign Out</button>
-        </div>
+        </div> */}
         <ToastContainer
           position="top-left"
           autoClose={2000}
